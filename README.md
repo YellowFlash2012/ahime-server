2defc7716cc49b45a103634f7801e7e7a296e4a4
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-30 05:07:47 +0100

    used a seeder script to automatically add all products to the db with a single command line!

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ https://github.com/YellowFlash2012/ahime-server.git
 Key learning points:
 1- learned how to use "concurrently" to run both client and server from a single command line
 2- how to turn nodejs imports into es5 module imports by adding "type":"module" to the package.json file
+3- learned how to use the data seeder script to create a batch of products in the db with one command line. Never heard of it before but now when I come tot hink of it, it makes sense. Adding products manually to the db must be painful especially when we are dealing with hundreds of products!
---
+++ seeders.js
@@ -0,0 +1,84 @@
+import express from "express";
+
+import products from "./data/products.js";
+import users from "./data/users.js";
+import Products from "./models/Products.js";
+import Orders from "./models/Orders.js";
+import Users from "./models/Users.js";
+import mongoose from "mongoose";
+import colors from "colors";
+import { config } from "dotenv";
+
+const app = express();
+
+config();
+
+// mongoose connection script
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(
+        app.listen(PORT, () => {
+            console.log(
+                `Server running in ${process.env.NODE_ENV} mode | Port ${PORT}`
+                    .yellow.bold
+            );
+
+            console.log("DB connected!".cyan.underline.bold);
+        })
+    )
+    .catch((err) => {
+        console.log(err.red.bold);
+    });
+
+// *****import script*****
+const importData = async () => {
+    try {
+        // clear the db
+        await Orders.deleteMany();
+        await Products.deleteMany();
+        await Users.deleteMany();
+
+        // create admin
+        const createdUsers = await Users.insertMany(users);
+        const adminUser = createdUsers[0]._id;
+
+        // add admin user to the products
+        const sampleProducts = products.map((p) => {
+            return { ...p, user: adminUser };
+        });
+
+        // create products
+        await Products.insertMany(sampleProducts);
+
+        console.log("Data imported!".green.inverse);
+
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
+};
+
+const destroyData = async () => {
+    try {
+        // clear the db
+        await Orders.deleteMany();
+        await Products.deleteMany();
+        await Users.deleteMany();
+
+        console.log("Data destroyed!".red.inverse);
+
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
+};
+
+if (process.argv[2] === "-d") {
+    destroyData();
+} else {
+    importData();
+}
