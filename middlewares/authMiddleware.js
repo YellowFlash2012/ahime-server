bd575487f59e37054c2795ce0c9939ac1f305a77
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-31 11:13:17 +0100

    generated a jwt token, created the auth middleware, finished the profile route and tested it with postman

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import Users from '../models/Users.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(" ")[1];
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await Users.findById(decoded.id).select('-password');
+
+            next();
+
+        } catch (error) {
+            console.error(error);
+            res.status(401)
+            throw new Error('Not authorized, token failed')
+        }
+        
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error('Area 51: Do not trespass!!')
+    }
+    
+
+    
+})
+
+export { protect };
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 
 import express from "express";
 import asyncHandler from "express-async-handler";
+import { protect } from "../middlewares/authMiddleware.js";
 import Users from "../models/Users.js";
+import generateToken from "../utils/generateToken.js";
 const router = express.Router();
 
 // @desc    Auth user & get token
@@ -18,11 +20,32 @@ router.post("/login", asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token:null
+            token:generateToken(user._id)
         })
     } else {
         res.status(401);
         throw new Error('Invalid credentials');
+    }
+}))
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+router.get("/profile", protect, asyncHandler(async (req, res) => {
+
+    // res.send('Success')
+    const user = await Users.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found')
     }
 }))
 
