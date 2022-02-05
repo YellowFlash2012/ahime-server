26df1048b9fd95468b4125c12e4ddcd9cd12e9fc
YellowFlash2012 <cyclop9@hotmail.com>
2022-02-05 06:47:18 +0100

    create a new product, update a product routes completed and tested with postman

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler';
+
+import { admin, protect } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
 import Products from '../models/Products.js'
@@ -43,6 +46,65 @@ router.delete("/:id", protect, admin, asyncHandler(async(req, res) => {
         res.status(404)
         throw new Error('Product not found!')
     }
+
+}));
+
+// @desc    create a product
+// @route   POST /api/products
+// @access  Private/Admin
+router.post("/", protect, admin, asyncHandler(async(req, res) => {
+    const product = new Products({
+        name: 'sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'sample brand',
+        category: 'sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'sample description'
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+
+
+}));
+
+// @desc    update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+router.put("/:id", protect, admin, asyncHandler(async(req, res) => {
+    const {
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        description
+    } = req.body;
+
+    const product = await Products.findById(req.params.id);
+
+    if (product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
+
+        const updatedProduct = await product.save();
+        res.status(201).json(updatedProduct);
+    } else {
+        res.status(404);
+        throw new Error('Product not found')
+    }
+
+    
+
 
 }));
 
