b801e5e3b44f6d4c26b17eb8073713a1a9813b18
YellowFlash2012 <cyclop9@hotmail.com>
2022-02-07 17:39:18 +0100

    last preps before deployment

--- server.js
+++ server.js
@@ -23,9 +23,6 @@ if (process.env.NODE_ENV === "development") {
 // to accept json data
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-})
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -40,6 +37,17 @@ app.get('/api/config/paypal', (req, res) => {
 const __dirname = path.resolve();
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// serving the FE with the index.html
+if (process.env.NODE_ENV==='production') {
+    app.use(express.static(path.join(__dirname, '../ahime-client/build')));
+
+    app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname, 'ahime-client', 'build', 'index.html')))
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running");
+    });
+}
 
 // custom middleware for NOT FOUND error handling
 app.use(notFound)
